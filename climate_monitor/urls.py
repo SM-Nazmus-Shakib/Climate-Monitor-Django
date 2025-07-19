e4f8b01479f4f0871f32f6ef8592027d91b35975
Arnob <nzsk0990@gmail.com>
2025-07-19 20:33:39 +0600

    3rd commit

--- climate_monitor/urls.py
+++ climate_monitor/urls.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    # Include other apps' URLs here
     path('weather/', include('weather.urls')),
     path('farms/', include('farms.urls')),
     path('damage-reports/', include('damage_reports.urls')),
     path('users/', include('users.urls')),
-    path('', include('farms.urls')),  # Default to farms map
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')  
+
+def no_permission(request):
+    return render(request, 'core/no_permission.html')
+
+@login_required
+def climate_dashboard(request):
+    return render(request, 'core/climate_dashboard.html')
